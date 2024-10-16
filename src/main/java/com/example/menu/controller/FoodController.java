684f1f0ffca574ee975d0f47131d994cf2e4b8a6
Miguel-Kwasinski <mvkwasinski@gmail.com>
2024-10-16 14:11:37 -0300

    add post method

--- src/main/java/com/example/menu/controller/FoodController.java
+++ src/main/java/com/example/menu/controller/FoodController.java
@@ -2,11 +2,10 @@ package com.example.menu.controller;
 
 import com.example.menu.Food.Food;
 import com.example.menu.Food.FoodRepository;
+import com.example.menu.Food.FoodRequestDTO;
 import com.example.menu.Food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +16,15 @@ public class FoodController {
     @Autowired
     private FoodRepository repository;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data) {
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<FoodResponseDTO> getAll(){
         List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
