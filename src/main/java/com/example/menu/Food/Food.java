57e100c8d7e9854d12954db55b34ffdb951791dd
Miguel-Kwasinski <mvkwasinski@gmail.com>
2024-10-16 13:43:36 -0300

    adjust get method

--- src/main/java/com/example/menu/Food/Food.java
+++ src/main/java/com/example/menu/Food/Food.java
@@ -1,49 +1,19 @@
 package com.example.menu.Food;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 @Table(name = "foods")
-@Entity
+@Entity(name = "foods")
 public class Food {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String title;
-    private String image;
+    private  String title;
+    private  String image;
     private Integer price;
-
-    // Getters e Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
 }
--- src/main/java/com/example/menu/controller/FoodController.java
+++ src/main/java/com/example/menu/controller/FoodController.java
@@ -2,6 +2,7 @@ package com.example.menu.controller;
 
 import com.example.menu.Food.Food;
 import com.example.menu.Food.FoodRepository;
+import com.example.menu.Food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,10 @@ public class FoodController {
     @Autowired
     private FoodRepository repository;
 
+
     @GetMapping
-    public List<Food> getAll(){
-        List<Food> foodList = repository.findAll();
+    public List<FoodResponseDTO> getAll(){
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
         return foodList;
     }
 }
